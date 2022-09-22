b49e95195b6aee241f4ab6974390b72d5a0dd473
Rena Kunisaki <i.am.inuyasha@gmail.com>
2022-09-22 10:29:12 -0400

    fix negative branch hooks

--- src/hook.c
+++ src/hook.c
@@ -16,9 +16,9 @@ uint32_t hookBranch(uint32_t addr, void *target, int isBl) {
     uint32_t oldOp = *(uint32_t*)addr;
     //make b or bl opcode
     uint32_t relDest = (uint32_t)target - addr;
-    *(uint32_t*)addr = (relDest & 0x07FFFFFC) | (isBl ? 1 : 0) | 0x48000000;
+    *(uint32_t*)addr = (relDest & 0x03FFFFFC) | (isBl ? 1 : 0) | 0x48000000;
     iCacheFlush((void*)addr, 4);
 
     //decode original instruction and return original destination
-    return addr + (oldOp & 0x07FFFFFC);
+    return addr + (oldOp & 0x03FFFFFC);
 }
