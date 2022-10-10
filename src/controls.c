b068ab5de99a3d35e1dbfc60787bd46e707b34e1
Rena Kunisaki <i.am.inuyasha@gmail.com>
2022-10-10 17:25:44 -0400

    tidying, bike controls

--- src/controls.c
+++ src/controls.c
@@ -24,6 +24,105 @@ void displayControllerState(int iPad, PADStatus *pad) {
     debugPrintf("L%3d R%3d\n", pad->triggerLeft, pad->triggerRight);
 }
 
+void displayWiimoteState(GameWiimoteState *wp) {
+    debugPrintf(DPRINT_FIXED " B%3d%% IR%d, %d, %dcm ang%d ",
+        (int)(((float)wp->battery / 255.0f) * 100.0f),
+        (int)wp->ir[0], (int)wp->ir[1], (int)(wp->ir[2] * 100.0f),
+        (int)(wp->irAngle));
+    debugPrintf("A %3d,%3d,%3d ",
+        (int)wp->accel[0], (int)wp->accel[1], (int)wp->accel[2]);
+    debugPrintf("O %3d,%3d,%3d ",
+        (int)wp->orient[0], (int)wp->orient[1], (int)wp->orient[2]);
+    debugPrintf("G %3d,%3d,%3d = %d ",
+        (int)wp->gforce[0], (int)wp->gforce[1], (int)wp->gforce[2],
+        (int)(ABS(wp->gforce[0])+
+            ABS(wp->gforce[1])+
+            ABS(wp->gforce[2]))
+        );
+
+    switch(wp->expType) {
+        case WPAD_EXP_NUNCHUK: {
+            debugPrintf("\nNC: J %3d,%3d A %3d,%3d,%3d ",
+                wp->exp.nunchuk.joystick[0],
+                wp->exp.nunchuk.joystick[1],
+                (int)wp->exp.nunchuk.accel[0],
+                (int)wp->exp.nunchuk.accel[1],
+                (int)wp->exp.nunchuk.accel[2]);
+            debugPrintf("O %3d,%3d,%3d ",
+                (int)wp->exp.nunchuk.orient[0],
+                (int)wp->exp.nunchuk.orient[1],
+                (int)wp->exp.nunchuk.orient[2]);
+            debugPrintf("G %3d,%3d,%3d = %d\n",
+                (int)(wp->exp.nunchuk.gforce[0] * 100.0f),
+                (int)(wp->exp.nunchuk.gforce[1] * 100.0f),
+                (int)(wp->exp.nunchuk.gforce[2] * 100.0f),
+                (int)(ABS(wp->exp.nunchuk.gforce[0])+
+                    ABS(wp->exp.nunchuk.gforce[1])+
+                    ABS(wp->exp.nunchuk.gforce[2]))
+                );
+            break;
+        }
+        case WPAD_EXP_CLASSIC: {
+            debugPrintf("JL %3d,%3d JR %3d,%3d L%3d R%3d\n",
+                wp->exp.classic.lStick[0],
+                wp->exp.classic.lStick[1],
+                wp->exp.classic.rStick[0],
+                wp->exp.classic.rStick[1],
+                wp->exp.classic.lTrig,
+                wp->exp.classic.rTrig);
+            break;
+        }
+        default: break;
+    }
+    debugPrintf("\n" DPRINT_NOFIXED);
+}
+
+void adjustPlayerRotation(s16 x, s16 y) {
+    pPlayer->pos.rotation.x -= x;
+    pPlayer->pos.rotation.y -= y;
+    *(s16*)(pPlayer->state + 0x01A) -= x;
+    *(s16*)(pPlayer->state + 0x478) -= x;
+    *(s16*)(pPlayer->state + 0x484) -= x;
+    *(s16*)(pPlayer->state + 0x492) -= x;
+    *(s16*)(pPlayer->state + 0x494) -= x;
+    *(s32*)(pPlayer->state + 0x5C0) -= x;
+    ObjInstance *ride = *(ObjInstance**)(pPlayer->state + 0x7F0);
+    if(ride) {
+        ride->pos.rotation.x -= x;
+        ride->pos.rotation.y -= y;
+        if(ride->catId == 0x2E) { //bike
+            void *rState = ride->state;
+            *(s16*)(rState+0x40C) -= x;
+            *(s16*)(rState+0x40E) -= x;
+            *(s16*)(rState+0x41C) -= y;
+        }
+    }
+}
+
+u32 mapNunchukButtons(GameWiimoteState *wp, u32 btns) {
+    u32 result = 0;
+    if(btns & WPAD_NUNCHUK_BUTTON_Z) result |= PAD_TRIGGER_L;
+    if(btns & WPAD_NUNCHUK_BUTTON_C) result |= PAD_BUTTON_Y;
+    return result;
+}
+
+u32 mapClassicButtons(GameWiimoteState *wp, u32 btns) {
+    u32 result = 0;
+    //Nintendo pls what is consistency
+    if(btns & WPAD_CLASSIC_BUTTON_A)     result |= PAD_BUTTON_X;
+    if(btns & WPAD_CLASSIC_BUTTON_B)     result |= PAD_BUTTON_A;
+    if(btns & WPAD_CLASSIC_BUTTON_X)     result |= PAD_BUTTON_Y;
+    if(btns & WPAD_CLASSIC_BUTTON_Y)     result |= PAD_BUTTON_B;
+    if(btns & WPAD_CLASSIC_BUTTON_ZR)    result |= PAD_TRIGGER_Z;
+    if(btns & WPAD_CLASSIC_BUTTON_PLUS)  result |= PAD_BUTTON_START;
+    if(btns & WPAD_CLASSIC_BUTTON_HOME)  result |= PAD_BUTTON_START;
+    if(btns & WPAD_CLASSIC_BUTTON_UP)    result |= PAD_BUTTON_UP;
+    if(btns & WPAD_CLASSIC_BUTTON_DOWN)  result |= PAD_BUTTON_DOWN;
+    if(btns & WPAD_CLASSIC_BUTTON_LEFT)  result |= PAD_BUTTON_LEFT;
+    if(btns & WPAD_CLASSIC_BUTTON_RIGHT) result |= PAD_BUTTON_RIGHT;
+    return result;
+}
+
 u32 mapWiimoteButtons(GameWiimoteState *wp, u32 btns) {
     //generate GC button mask corresponding to Wii button
     //state on given pad.
@@ -31,26 +130,11 @@ u32 mapWiimoteButtons(GameWiimoteState *wp, u32 btns) {
     u32 result = 0;
     switch(wp->expType) {
         case WPAD_EXP_NUNCHUK: {
-                if(btns & WPAD_NUNCHUK_BUTTON_Z)
-                    result |= PAD_TRIGGER_L;
-                if(btns & WPAD_NUNCHUK_BUTTON_C)
-                    result |= PAD_BUTTON_Y;
-                break;
+            result |= mapNunchukButtons(wp, btns);
             break;
         }
         case WPAD_EXP_CLASSIC: {
-            //Nintendo pls what is consistency
-            if(btns & WPAD_CLASSIC_BUTTON_A)     result |= PAD_BUTTON_X;
-            if(btns & WPAD_CLASSIC_BUTTON_B)     result |= PAD_BUTTON_A;
-            if(btns & WPAD_CLASSIC_BUTTON_X)     result |= PAD_BUTTON_Y;
-            if(btns & WPAD_CLASSIC_BUTTON_Y)     result |= PAD_BUTTON_B;
-            if(btns & WPAD_CLASSIC_BUTTON_ZR)    result |= PAD_TRIGGER_Z;
-            if(btns & WPAD_CLASSIC_BUTTON_PLUS)  result |= PAD_BUTTON_START;
-            if(btns & WPAD_CLASSIC_BUTTON_HOME)  result |= PAD_BUTTON_START;
-            if(btns & WPAD_CLASSIC_BUTTON_UP)    result |= PAD_BUTTON_UP;
-            if(btns & WPAD_CLASSIC_BUTTON_DOWN)  result |= PAD_BUTTON_DOWN;
-            if(btns & WPAD_CLASSIC_BUTTON_LEFT)  result |= PAD_BUTTON_LEFT;
-            if(btns & WPAD_CLASSIC_BUTTON_RIGHT) result |= PAD_BUTTON_RIGHT;
+            result |= mapClassicButtons(wp, btns);
             return result; //do not use the regular Wiimote buttons
         }
         default: break;
@@ -104,6 +188,85 @@ bool applyAimToStaff(GameWiimoteState *wp, PADStatus *pad) {
     }
 }
 
+void applyJoystickInputs(GameWiimoteState *wp, PADStatus *pad) {
+    switch(wp->expType) {
+        case WPAD_EXP_NUNCHUK: {
+            pad->stickX = wp->exp.nunchuk.joystick[0];
+            pad->stickY = wp->exp.nunchuk.joystick[1];
+            break;
+        }
+        case WPAD_EXP_CLASSIC: {
+            pad->stickX       = wp->exp.classic.lStick[0];
+            pad->stickY       = wp->exp.classic.lStick[1];
+            pad->substickX    = wp->exp.classic.rStick[0];
+            pad->substickY    = wp->exp.classic.rStick[1];
+            pad->triggerLeft  = wp->exp.classic.lTrig;
+            pad->triggerRight = wp->exp.classic.rTrig;
+            break;
+        }
+        default: break;
+    }
+}
+
+void doSwingGestures(GameWiimoteState *wp, u32 *bDown) {
+    s16 stateNo = *(s16*)(pPlayer->state+0x274);
+
+    //swing to roll
+    if(wp->expType == WPAD_EXP_NUNCHUK && (
+    ABS(wp->exp.nunchuk.gforce[0]) +
+    ABS(wp->exp.nunchuk.gforce[1]) +
+    ABS(wp->exp.nunchuk.gforce[2]) >= 1.1f)) {
+        OSReport("rollin' rollin' rollin'\n");
+        *bDown |= PAD_BUTTON_X;
+    }
+
+    //swing to attack
+    switch(stateNo) {
+        case 0x01: //idle
+        case 0x02: { //moving
+            if(*(u8*)(pPlayer->state+0x8B3) != 0) { //staff in hand
+                if(ABS(wp->gforce[0]) +
+                ABS(wp->gforce[1]) +
+                ABS(wp->gforce[2]) >= 8.0f) {
+                    OSReport("swing it!\n");
+                    *bDown |= PAD_BUTTON_A;
+                }
+            }
+            break;
+        }
+        case 0x18: { //riding a bike
+            if(wp->expType == WPAD_EXP_NUNCHUK) {
+                //XXX is the orientation sensor broken
+                //or is it normal that X is always 0?
+                adjustPlayerRotation(
+                    wp->exp.nunchuk.orient[2] * 12,
+                    wp->exp.nunchuk.orient[1] * 10);
+                //maybe forward tilt can be accel/brake instead
+            }
+            break;
+        }
+        default: break;
+    }
+}
+
+void doAimControls(PADStatus *pad) {
+    //try to let player walk around in this state.
+    //this doesn't work...
+
+    //clear flags that don't allow player to move
+    /*(*(u32*)(pPlayer->state + 0)) = 0;
+    (*(u8*) (pPlayer->state + 0x34C)) = 0;
+    (*(u8*) (pPlayer->state + 0x360)) = 0;
+    //void (*func)(
+    //    ObjInstance *player, void *state, void *state2) = 0x802b0ea4;
+    //func(pPlayer, pPlayer->state, pPlayer->state);
+    void (*func)(
+        ObjInstance *player, void *state) = 0x802a514c;
+    func(pPlayer, pPlayer->state);*/
+
+    adjustPlayerRotation(pad->stickX * 16, pad->stickY * 16);
+}
+
 static u8 prevWiimoteFlags[4];
 void applyWiimoteInputs(int iPad, PADStatus *state) {
     //only called when the Wiimote is connected.
@@ -135,124 +298,16 @@ void applyWiimoteInputs(int iPad, PADStatus *state) {
         //the digital input doesn't trigger the shield
         state->triggerRight = 255;
     }
-
+    applyJoystickInputs(wp, state);
     if(isAim) {
         //HACK: don't calculate staff aim coordinates
         WRITE32(0x8029b1fc, 0x60000000);
         WRITE32(0x8029b270, 0x60000000);
         WRITE32(0x8029b238, 0x60000000);
     }
-
-    debugPrintf(DPRINT_FIXED " B%3d%% IR%d, %d, %dcm ang%d ",
-        (int)(((float)wp->battery / 255.0f) * 100.0f),
-        (int)wp->ir[0], (int)wp->ir[1], (int)(wp->ir[2] * 100.0f),
-        (int)(wp->irAngle));
-    debugPrintf("A %3d,%3d,%3d ",
-        (int)wp->accel[0], (int)wp->accel[1], (int)wp->accel[2]);
-    debugPrintf("O %3d,%3d,%3d ",
-        (int)wp->orient[0], (int)wp->orient[1], (int)wp->orient[2]);
-    debugPrintf("G %3d,%3d,%3d = %d ",
-        (int)wp->gforce[0], (int)wp->gforce[1], (int)wp->gforce[2],
-        (int)(ABS(wp->gforce[0])+
-            ABS(wp->gforce[1])+
-            ABS(wp->gforce[2]))
-        );
-
-    //joystick inputs
-    switch(wp->expType) {
-        case WPAD_EXP_NUNCHUK: {
-            debugPrintf("\nNC: J %3d,%3d A %3d,%3d,%3d ",
-                wp->exp.nunchuk.joystick[0],
-                wp->exp.nunchuk.joystick[1],
-                (int)wp->exp.nunchuk.accel[0],
-                (int)wp->exp.nunchuk.accel[1],
-                (int)wp->exp.nunchuk.accel[2]);
-            debugPrintf("O %3d,%3d,%3d ",
-                (int)wp->exp.nunchuk.orient[0],
-                (int)wp->exp.nunchuk.orient[1],
-                (int)wp->exp.nunchuk.orient[2]);
-            debugPrintf("G %3d,%3d,%3d = %d\n",
-                (int)(wp->exp.nunchuk.gforce[0] * 100.0f),
-                (int)(wp->exp.nunchuk.gforce[1] * 100.0f),
-                (int)(wp->exp.nunchuk.gforce[2] * 100.0f),
-                (int)(ABS(wp->exp.nunchuk.gforce[0])+
-                    ABS(wp->exp.nunchuk.gforce[1])+
-                    ABS(wp->exp.nunchuk.gforce[2]))
-                );
-                state->stickX = wp->exp.nunchuk.joystick[0];
-                state->stickY = wp->exp.nunchuk.joystick[1];
-
-            //swing to roll
-            if(ABS(wp->exp.nunchuk.gforce[0]) +
-               ABS(wp->exp.nunchuk.gforce[1]) +
-               ABS(wp->exp.nunchuk.gforce[2]) >= 1.5f) {
-                OSReport("rollin' rollin' rollin'\n");
-                bDown |= PAD_BUTTON_X;
-            }
-            break;
-        }
-        case WPAD_EXP_CLASSIC: {
-            debugPrintf("JL %3d,%3d JR %3d,%3d L%3d R%3d\n",
-                wp->exp.classic.lStick[0],
-                wp->exp.classic.lStick[1],
-                wp->exp.classic.rStick[0],
-                wp->exp.classic.rStick[1],
-                wp->exp.classic.lTrig,
-                wp->exp.classic.rTrig);
-            state->stickX       = wp->exp.classic.lStick[0];
-            state->stickY       = wp->exp.classic.lStick[1];
-            state->substickX    = wp->exp.classic.rStick[0];
-            state->substickY    = wp->exp.classic.rStick[1];
-            state->triggerLeft  = wp->exp.classic.lTrig;
-            state->triggerRight = wp->exp.classic.rTrig;
-            break;
-        }
-        default:
-            break;
-    }
-    debugPrintf("\n" DPRINT_NOFIXED);
-
-    s16 stateNo = *(s16*)(pPlayer->state+0x274);
-    switch(stateNo) {
-        case 0x01: //idle
-        case 0x02: { //moving
-            if(*(u8*)(pPlayer->state+0x8B3) != 0) { //staff in hand
-                if(ABS(wp->gforce[0]) +
-                ABS(wp->gforce[1]) +
-                ABS(wp->gforce[2]) >= 8.0f) {
-                    OSReport("swing it!\n");
-                    bDown |= PAD_BUTTON_A;
-                }
-            }
-            break;
-        }
-        default: break;
-    }
-
-    if(isAim) {
-        //try to let player walk around in this state.
-        //this doesn't work...
-
-        //clear flags that don't allow player to move
-        /*(*(u32*)(pPlayer->state + 0)) = 0;
-        (*(u8*) (pPlayer->state + 0x34C)) = 0;
-        (*(u8*) (pPlayer->state + 0x360)) = 0;
-        //void (*func)(
-        //    ObjInstance *player, void *state, void *state2) = 0x802b0ea4;
-        //func(pPlayer, pPlayer->state, pPlayer->state);
-        void (*func)(
-            ObjInstance *player, void *state) = 0x802a514c;
-        func(pPlayer, pPlayer->state);*/
-
-        //pPlayer->pos.rotation.x += state->stickX;
-        pPlayer->pos.rotation.y -= state->stickY * 16;
-        *(s16*)(pPlayer->state + 0x01A) -= state->stickX * 16;
-        *(s16*)(pPlayer->state + 0x478) -= state->stickX * 16;
-        *(s16*)(pPlayer->state + 0x484) -= state->stickX * 16;
-        *(s16*)(pPlayer->state + 0x492) -= state->stickX * 16;
-        *(s16*)(pPlayer->state + 0x494) -= state->stickX * 16;
-        *(s32*)(pPlayer->state + 0x5C0) -= state->stickX * 16;
-    }
+    displayWiimoteState(wp);
+    doSwingGestures(wp, &bDown);
+    if(isAim) doAimControls(state);
     state->button = bHeld | bDown;
 }
 
